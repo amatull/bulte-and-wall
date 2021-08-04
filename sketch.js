49abbe396ca848692b831594f57821f4fa856a77
amatull <86194076+amatull@users.noreply.github.com>
2021-08-04 11:57:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,38 @@
+var car, wall;
+var speed,weight;
+
+
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createSprite(1600,400);
+
+car = createSprite(50,200,50,50);
+wall=createSprite(1200,200,60,100  )
+
+   speed=random(55,90);
+   weight=random(400,1500);
+
+car .velocityX=speed;
 }
 
 function draw() {
   background(255,255,255);  
-  drawSprites();
+  
+if(wall.x-car.x< (car.width+wall.width)/2)
+{
+  car.velocityX=0;
+var defromation=0.5 * weight * speed * speed/25500;
+if(defromation>180){
+  car.shapeColor="red";
+}
+if(defromation<180 &&defromation<100){
+  car.shapeColor="yellow";
+}
+if(defromation>100){
+car.shapeColor="green";
+}
+
+}
+drawSprites();
+
 }
